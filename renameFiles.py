26c6e1d8eda97c4738f5235ef11a109b931b977d
Eric Kramer <eric.kramer@showtime.net>
2016-03-03 13:20:56 -0500

    default logic

--- renameFiles.py
+++ renameFiles.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 # execute ./renameFiles.py PATH/TO/DIRECTORY STRING-TO-BE-REPLACED(optional) REPLACER(optional)
-# default replaces spaces with _'s if optional arguments are not supplied
 
 import sys
 print(sys.argv[1])
@@ -10,11 +9,19 @@ from os import listdir
 
 folder = sys.argv[1]
 file_list = listdir(folder)
-replace = sys.argv[2]
-if sys.argv[3] == ' ':
-	replacer = '\\ '
+
+# default replaces spaces with _'s if optional arguments are not supplied
+if len(sys.argv) > 2:
+	replace = sys.argv[2] 
+	# if you need to add spaces for some reason
+	if sys.argv[3] == ' ':
+		replacer = '\\ '
+	else:
+		replacer = sys.argv[3]
 else:
-	replacer = sys.argv[3]
+	replace = '\s'
+	replacer = '_'
+
 
 for file_name in file_list:
-	subprocess.call("mv " + folder + "/" + re.sub("\s", "\\ ", file_name) + " " + folder + "/" + re.sub(replace, replacer, file_name.lower()) if len(sys.argv) > 2 else re.sub("\s", "_", file_name.lower()), shell=True)
+	subprocess.call("mv " + folder + "/" + re.sub("\s", "\\ ", file_name) + " " + folder + "/" + re.sub(replace, replacer, file_name.lower()), shell=True)
